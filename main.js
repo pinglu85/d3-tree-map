8dbece35f998eb9a2baaa0e34c8a2dc5e4600a09
pinglu85 <luping8576@hotmail.com>
2020-05-01 23:59:19 +0200

    Finish basic tiles

--- main.js
+++ main.js
@@ -19,22 +19,62 @@ const DATASETS = {
       'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json',
   },
 };
+const DEFAULT_DATASET = 'videoGames';
 
 const title = document.getElementById('title');
 const description = document.getElementById('description');
 
-title.innerText = DATASETS.videoGames.title;
-description.innerText = DATASETS.videoGames.description;
+title.innerText = DATASETS[DEFAULT_DATASET].title;
+description.innerText = DATASETS[DEFAULT_DATASET].description;
 
 const width = 960;
 const height = 570;
 const marginBottom = 210;
+const fader = (color) => d3.interpolateRgb(color, '#fff')(0.2);
+const color = d3
+  .scaleOrdinal()
+  .range(
+    [
+      '#1f77b4',
+      '#aec7e8',
+      '#ff7f0e',
+      '#ffbb78',
+      '#2ca02c',
+      '#98df8a',
+      '#d62728',
+      '#9467bd',
+      '#8c564b',
+      '#e377c2',
+      '#7f7f7f',
+      '#bcbd22',
+      '#17becf',
+    ].map(fader)
+  );
+const format = d3.format(',d');
 
 const svg = d3
   .select('.tree-map')
   .append('svg')
   .attr('width', width)
   .attr('height', height + marginBottom);
+
+// Tree map
+const treemap = (data) =>
+  d3
+    .treemap()
+    .tile(d3.treemapResquarify)
+    .size([width, height])
+    .round(true)
+    .paddingInner(1)(
+    d3
+      .hierarchy(data)
+      .eachBefore(
+        (d) =>
+          (d.data.id = (d.parent ? `${d.parent.data.id}.` : '') + d.data.name)
+      )
+      .sum((d) => d.value)
+      .sort((a, b) => b.value - a.value)
+  );
 
 // tooltip
 const tip = d3
@@ -53,17 +93,37 @@ svg.call(tip);
     description.innerText = DATASETS[datasetName].description;
     d3.json(DATASETS[datasetName].url)
       .then((data) => {
-        svg.selectAll('*').remove();
         ready(data);
       })
       .catch((err) => console.log(err));
   });
 });
 
-d3.json(DATASETS.videoGames.url)
+d3.json(DATASETS[DEFAULT_DATASET].url)
   .then((data) => ready(data))
   .catch((err) => console.log(err));
 
 function ready(data) {
-  console.log(data);
+  const root = treemap(data);
+  const t = svg.transition().duration(200);
+
+  const leaf = svg
+    .selectAll('g')
+    .data(root.leaves())
+    .join('g')
+    .attr('transform', (d) => `translate(${d.x0}, ${d.y0})`);
+
+  // Remove the existing children from leaf
+  leaf.selectAll('*').transition().duration(300).style('opacity', 0).remove();
+
+  leaf
+    .append('rect')
+    .attr('id', (d) => d.data.id)
+    .attr('class', 'tile')
+    .attr('data-name', (d) => d.data.name)
+    .attr('data-category', (d) => d.data.category)
+    .attr('data-value', (d) => d.data.value)
+    .attr('fill', (d) => color(d.data.category))
+    .attr('width', (d) => d.x1 - d.x0)
+    .attr('height', (d) => d.y1 - d.y0);
 }
